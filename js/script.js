ef7a58580d53ccfe816db816901c95e367aeaa6f
Nahuel Benitez <benitez-serg10@hotmail.com>
2022-10-24 23:02:06 -0300

    Database

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.getElementById("sqlite").addEventListener("click", ()=> {
+   window.location = "sqlite.html"
+})
+
+document.getElementById("mcsqlsv").addEventListener("click", ()=> {
+    window.location = "sqlserver.html"
+ })
+
+ document.getElementById("mysql").addEventListener("click", ()=> {
+    window.location = "mysql.html"
+ })
+
+ document.getElementById("mariadb").addEventListener("click", ()=> {
+    window.location = "mariadb.html"
+ })
+
+ document.getElementById("oracledb").addEventListener("click", ()=> {
+    window.location = "oracle.html"
+ })
+
+ document.getElementById("access").addEventListener("click", ()=> {
+    window.location = "access.html"
+ })
